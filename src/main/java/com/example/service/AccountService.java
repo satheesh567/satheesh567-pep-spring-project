734d62ddf161ebb1358667957c4891c6fd5317b7
satheesh567 <chaitanyasugithi@gmail.com>
2023-12-02 23:50:16 +0530

    spring pep project

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,39 @@
 package com.example.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
 public class AccountService {
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+     ObjectMapper objectMapper;
+   
+    public Account saveAccount(Account account){
+       
+        //  Account accountEntity=objectMapper.convertValue(userDto, Account.class);
+        //   System.out.println(accountEntity);
+         return accountRepository.save(account);
+        
+
+    }
+    public boolean findUsername(String username){
+        Optional<Account> findByUsername = accountRepository.findByUsername(username);
+        if(findByUsername.isPresent()){
+            return true;
+        }
+        return false;
+    }
+
+     public Account logIn(Account account){
+        return accountRepository.findByUsernameAndPassword(account.getUsername(), account.getPassword());
+     }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,4 +1,63 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.repository.AccountRepository;
+import com.example.repository.MessageRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
 public class MessageService {
+    @Autowired
+    MessageRepository messageRepository;
+    @Autowired
+    AccountRepository accountRepository;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    public Message createMessage(Message message){
+        if(isValidMessage(message)){
+    
+        return messageRepository.save(message);
+        }
+        else {
+            throw new IllegalArgumentException("Invalid message parameters");
+        }
+        
+    }
+
+    private boolean isValidMessage(Message message) {
+        return message.getMessage_text() != null &&
+               !message.getMessage_text().isBlank() &&
+               message.getMessage_text().length() <= 255 &&
+               message.getPosted_by() != null && accountRepository.findAccountByAccountId(message.getPosted_by())!=null; // You might want to perform additional validation for posted_by
+               /* Add more validation as needed */
+    }
+
+    public List<Message> getAllMessages(){
+        return  messageRepository.findAll();
+    }
+
+    public Message getMessageById(Integer id){
+        return messageRepository.findMessageMessageId(id);
+    }
+
+    public Integer deleteByMessageId(Integer messageId){
+        return messageRepository.deleteByMessageIdAndReturnCount(messageId);
+    }
+
+    public Integer updateMessage(Integer messageId,String messaText){
+        return messageRepository.updateMessageTextByMessageId(messageId, messaText);
+    }
+    public List<Message> findMessageByUser(Integer id){
+       return  messageRepository.findByUser(id);
+    }
+
+
 }
+
